3fb703d5191b8fd57414d10b6f681c13f88485b1
Chalon Hutson <chalonhutson@gmail.com>
2023-08-02 13:54:53 -0600

    Added some code for using the csv module.

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+import csv
 FILENAME = "characters.csv"
 
-with open(FILENAME, "r") as file:
-    for character in file:
-        name, actor, role, description, seasons, status = character.split(",")
-        print(name)
+"""Using the standard file reader and split method."""
+# with open(FILENAME, "r") as file:
+#     for character in file:
+#         name, actor, role, description, seasons, status = character.split(",")
+#         print(name)
 
-    # print([len(character.rstrip().split(",")) for character in file])
+"""Using the standard CSV Reader."""
+# with open(FILENAME) as file:
+#     for character in csv.reader(file):
+#         character_name = character[0]
+#         print(character_name)
+
+"""Using the DictReader within the CSV module."""
+# with open(FILENAME) as file:
+#     dict_reader = csv.DictReader(file)
+
+#     for character in dict_reader:
+#         print(character["name"])
